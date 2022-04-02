8b38dc409dd5f20509f137b35e9fd5fd83935927
Charlie Smurthwaite <netmaster10000@yahoo.com>
2022-04-02 19:25:43 +0100

    add simple FIR filter to encoder input, add comments for planning

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ void SystemInit (void)
   RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
   // Set PORTA modes.
   GPIOA->CRL = GPIO_CRL_CNF6_1 | GPIO_CRL_CNF7_1;
-  GPIOA->ODR = 0xFFFFFFFF;
   GPIOA->CRH = GPIO_CRH_MODE8_0 | GPIO_CRH_MODE8_1 | GPIO_CRH_CNF8_1 | GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1 | GPIO_CRH_CNF9_1 | GPIO_CRH_MODE10_0 | GPIO_CRH_MODE10_1 | GPIO_CRH_CNF10_1;
+  GPIOA->ODR = 0xFFFFFFFF;
   // Set PORTB modes.
   GPIOB->CRH = GPIO_CRH_MODE13_0 | GPIO_CRH_MODE13_1 | GPIO_CRH_CNF13_1 | GPIO_CRH_MODE14_0 | GPIO_CRH_MODE14_1 | GPIO_CRH_CNF14_1 | GPIO_CRH_MODE15_0 | GPIO_CRH_MODE15_1 | GPIO_CRH_CNF15_1;
 
@@ -65,23 +65,40 @@ void SystemInit (void)
   TIM3->CCMR1 = TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_0;
   // Enable timer
   TIM3->CR1 |= TIM_CR1_CEN;
+
+  // TODO: Throttle ADC configuration
+  // TODO: PWM configuration for current limit outputs
+  // TODO: Digital inputs for direction and error states
+}
+
+// Storage of previous encoder deltas
+uint16_t encoder_deltas[256];
+uint8_t encoder_delta_pos;
+
+// One full rotation is 72 << 24 (1207959552)
+// This absurdly large  allows us to be reasonably precise without the need for FPU
+uint32_t stator_angle;
+
+// This function runs at 17.5kHz intervals and handles all calculations
+// required to generate the PWM sine wave outputs.
+void pwm_interval() {
+  // Cache encoder deltas to allow averaging
+  encoder_deltas[encoder_delta_pos++] = TIM3->CNT;
+  // Calculate sum of encoder deltas over previous 16 periods
+  uint16_t encoder_delta = encoder_deltas[encoder_delta_pos - 1] - encoder_deltas[encoder_delta_pos - 1 - 16]
+  // Increment stator angle by the encoder delta. A full rotation is be 72 << 24, we scale down by 16.
+  stator_angle += encoder_delta << 20;
+
+  // TODO: Add a small fixed slip towards zero to ensure the neutral state is torque toward stationary
+  // TODO: Direction input
+  // TODO: Throttle input
+  // TODO: Increment the angle by the throttle position
+  // TODO: Calculate voltage based on sum of encoder and throttle increments
+  // TODO: Current limit output
+  // TODO: Error state input
 }
 
 int main() {
   // Nothing to do yet
   while(1);
-  // During the main program execution, most work will be done by
-  // a 17.5Khz interrupt. This will advance the PWM sine wave output
-  // by the rotation indicated by the encoder, plus slip indicated
-  // by the throttle. It will additionally calculate the correct
-  // amplitude for the output based on a V/Hz ratio.
-
-  // A major challenge is that in order to calculate V/Hz, we need
-  // to calculate the instantaneous rotational velocity.
-  // Unfortunately this means we need to make a decision as to wether
-  // we use TIM3 as an encoder input, which will not give us velovity
-  // or a timer, which will give us veocity, but not direction.
-
-  // We will use the same 17.5kHz interrupt to read ADC data (current,
-  // thrrote) and digital inputs (direction, fault states, etc).
 }
